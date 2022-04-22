12278e597cdf08a5af3312cd3e7398fcb6786428
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-04-22 05:37:03 +0000

    [Finish]4-1~24

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "homes#top"
+   get 'homes/about', as: 'about'
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+
+end
